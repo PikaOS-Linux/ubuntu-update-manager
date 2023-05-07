bf3076d432aed1fe744eead75750f79e9d892c1f
Ward Nakchbandi (Cosmic Fusion) <hotrod.master@hotmail.com>
2023-05-08 01:28:02 +0300

    hah

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 # Sign the packages
-dpkg-sig --sign builder ./output/pika-kde-desktop*.deb
+dpkg-sig --sign builder ./output/*.deb
 
 # Pull down existing ppa repo db files etc
 rsync -azP --exclude '*.deb' ferreo@direct.pika-os.com:/srv/www/pikappa/ ./output/repo
