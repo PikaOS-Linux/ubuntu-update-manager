daebaad2193cd6794b659ea2840ef983aa519370
Ward Nakchbandi (Cosmic Fusion) <hotrod.master@hotmail.com>
2023-05-08 01:26:02 +0300

    hah

--- release.sh
+++ release.sh
@@ -5,7 +5,7 @@ dpkg-sig --sign builder ./output/pika-kde-desktop*.deb
 rsync -azP --exclude '*.deb' ferreo@direct.pika-os.com:/srv/www/pikappa/ ./output/repo
 
 # Add the new package to the repo
-reprepro -V --basedir ./output/repo/ includedeb lunar ./output/pika-kde-desktop*.deb
+reprepro -V --basedir ./output/repo/ includedeb lunar ./output/*.deb
 
 # Push the updated ppa repo to the server
 rsync -azP ./output/repo/ ferreo@direct.pika-os.com:/srv/www/pikappa/
